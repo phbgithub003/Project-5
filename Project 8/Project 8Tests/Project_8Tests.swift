ad7ed3564daef4c411bbdb5da2dd960ab136ee08
harshitp <palacheralaharshit@gmail.com>
2024-10-29 12:56:24 +0530

    Project 8

---
+++ Project 8/Project 8Tests/Project_8Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_8Tests.swift
+//  Project 8Tests
+//
+//  Created by Harshit Bhargava  on 29/10/24.
+//
+
+import Testing
+@testable import Project_8
+
+struct Project_8Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Project 8/Project 8UITests/Project_8UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Project_8UITestsLaunchTests.swift
+//  Project 8UITests
+//
+//  Created by Harshit Bhargava  on 29/10/24.
+//
+
+import XCTest
+
+final class Project_8UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
